cbbaf41ba56a20ee67de3857b02f70f97e864a9e
Renan Deswarte <renandeswarte@gmail.com>
2016-05-06 18:02:51 -0700

    feat: add font-awesome, move bootstrap from style assets, add logo icon to header

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import FontAwesome from 'react-fontawesome'
 
 function Header() {
   return (
     <div className="container">
+      <FontAwesome name='cloud' className='logo' />
       <h2>Weather App</h2>
     </div>
   )
